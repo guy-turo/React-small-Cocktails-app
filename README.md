4565112636d9e6eb4f925748be2f904e148a83de
guy-turo <kasongokakumbiguy@gmail.com>
2024-04-05 11:31:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# React-small-Cocktails-app" 
---
+++ app/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className='section about-section'>
+    <h1 className='section-title'>about us</h1>
+    <p>
+    Oh, yeah! It perfectly answers the question. However, make sure to notice the change in component to 'element', otherwise will result in a silent warning in the browser console "Matched leaf route at location "/" does not have an element. This means it will render an 
+    </p>
+   </section>
+  )
+}
+
+export default About
